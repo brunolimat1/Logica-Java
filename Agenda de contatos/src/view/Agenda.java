51127e41836a2877713c42336e599824f9137538
BRUNO DE LIMA SOARES <bruno.lsoares1@senacsp.edu.br>
2023-03-24 17:02:16 -0300

    Atualizando Agenda

--- Agenda de contatos/src/view/Agenda.java
+++ Agenda de contatos/src/view/Agenda.java
@@ -5,10 +5,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Toolkit;
+import java.awt.Window.Type;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Cursor;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 public class Agenda extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtID;
+	private JTextField txtNome;
+	private JTextField txtFone;
+	private JTextField txtEmail;
 
 	/**
 	 * Launch the application.
@@ -30,12 +47,136 @@ public class Agenda extends JFrame {
 	 * Create the frame.
 	 */
 	public Agenda() {
+		setTitle("Agenda de Contatos");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Agenda.class.getResource("/img/note.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 456, 410);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.GRAY);
+		contentPane.setBorder(null);
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblNewLabel_0 = new JLabel("ID:");
+		lblNewLabel_0.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		lblNewLabel_0.setBounds(45, 70, 48, 20);
+		contentPane.add(lblNewLabel_0);
+
+		txtID = new JTextField();
+		txtID.setBounds(93, 70, 40, 20);
+		contentPane.add(txtID);
+		txtID.setColumns(10);
+
+		JLabel lblNewLabel_1 = new JLabel("Nome:");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(41, 120, 60, 20);
+		contentPane.add(lblNewLabel_1);
+
+		txtNome = new JTextField();
+		txtNome.setBounds(93, 120, 267, 20);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+
+		JLabel lblNewLabel_2 = new JLabel("Fone:");
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		lblNewLabel_2.setBounds(41, 170, 60, 20);
+		contentPane.add(lblNewLabel_2);
+
+		txtFone = new JTextField();
+		txtFone.setBounds(93, 170, 150, 20);
+		contentPane.add(txtFone);
+		txtFone.setColumns(10);
+
+		JLabel lblNewLabel_3 = new JLabel("E-mail:");
+		lblNewLabel_3.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		lblNewLabel_3.setBounds(41, 220, 60, 20);
+		contentPane.add(lblNewLabel_3);
+
+		txtEmail = new JTextField();
+		txtEmail.setBounds(93, 220, 267, 20);
+		contentPane.add(txtEmail);
+		txtEmail.setColumns(10);
+
+		JButton btnEditar = new JButton("");
+		btnEditar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnEditar.setToolTipText("Editar Contato");
+		btnEditar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnEditar.setBorder(null);
+		btnEditar.setContentAreaFilled(false);
+		btnEditar.setIcon(new ImageIcon(Agenda.class.getResource("/img/edit3.png")));
+		btnEditar.setBounds(140, 300, 48, 48);
+		contentPane.add(btnEditar);
+
+		JButton btnExcluir = new JButton("");
+		btnExcluir.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnExcluir.setToolTipText("Excluir Contato");
+		btnExcluir.setContentAreaFilled(false);
+		btnExcluir.setBorder(null);
+		btnExcluir.setIcon(new ImageIcon(Agenda.class.getResource("/img/delete3.png")));
+		btnExcluir.setBounds(240, 300, 48, 48);
+		contentPane.add(btnExcluir);
+
+		JButton btnAdicionar = new JButton("");
+		btnAdicionar.setIcon(new ImageIcon(Agenda.class.getResource("/img/add3.png")));
+		btnAdicionar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnAdicionar.setToolTipText("Adicionar Contato");
+		btnAdicionar.setContentAreaFilled(false);
+		btnAdicionar.setBorder(null);
+		btnAdicionar.setBounds(40, 300, 48, 48);
+		contentPane.add(btnAdicionar);
+
+		JButton btnLimpar = new JButton("");
+		btnLimpar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnLimpar.setContentAreaFilled(false);
+		btnLimpar.setBorder(null);
+		btnLimpar.setIcon(new ImageIcon(Agenda.class.getResource("/img/trash3.png")));
+		btnLimpar.setToolTipText("Limpar Tudo");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limparCampos();
+			}
+		});
+		btnLimpar.setBounds(340, 300, 48, 48);
+		contentPane.add(btnLimpar);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		btnNewButton.setContentAreaFilled(false);
+		btnNewButton.setBorder(null);
+		btnNewButton.setIcon(new ImageIcon(Agenda.class.getResource("/img/search3.png")));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(380, 99, 41, 41);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Agenda.class.getResource("/img/dboff3.png")));
+		lblNewLabel.setBounds(10, 11, 24, 24);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton_1 = new JButton("");
+		btnNewButton_1.setToolTipText("Sobre");
+		btnNewButton_1.setContentAreaFilled(false);
+		btnNewButton_1.setBorder(null);
+		btnNewButton_1.setIcon(new ImageIcon(Agenda.class.getResource("/img/about3.png")));
+		btnNewButton_1.setBounds(406, 11, 24, 24);
+		contentPane.add(btnNewButton_1);
+	}// FIM DO CONSTRUTOR
+
+	/**
+	 * Método responsável por limpar os campos
+	 */
+	private void limparCampos() {
+		txtID.setText(null);
+		txtNome.setText(null);
+		txtFone.setText(null);
+		txtEmail.setText(null);
 	}
 
-}
+}// FIM DO CÓDIGO
